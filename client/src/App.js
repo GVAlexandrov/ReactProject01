a2f0f775c51c5bfb0e7c07c239ebe7700cea2e37
gvalexandrov <gvalexandrov@abv.bg>
2021-03-14 19:13:22 +0200

    Navigation bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import style from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="http://cska.bg/index.php"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          C S K A
-        </a>
-      </header>
+    <div className={style.App}>
+      <nav className={style.Navigation}>
+        <ul>
+          <li className="listItem"><img src="/white-origami-bird.png" alt="white origami"/></li>
+          <li className="listItem"><a href="#">Information</a></li>
+          <li className="listItem"><a href="#">Projects</a></li>
+          <li className="listItem"><a href="#">Going to 3</a></li>
+          <li className="listItem"><a href="#">Going to 4</a></li>
+          <li className="listItem"><a href="#">Going to 5</a></li>
+        </ul>
+
+      </nav>
     </div>
   );
 }
