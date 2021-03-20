3df6686b62e6ed6886ed801cbb7e6b693906afc0
gvalexandrov <gvalexandrov@abv.bg>
2021-03-20 14:39:39 +0200

    New modules

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,8 @@ function App() {
         <ul>
           <li className="listItem"><img src="/white-origami-bird.png" alt="white origami"/></li>
           <li className="listItem"><a href="#">Information</a></li>
-          <li className="listItem"><a href="#">Projects</a></li>
-          <li className="listItem"><a href="#">Going to 3</a></li>
+          <li className="listItem"><a href="#">Expenditures</a></li>
+          <li className="listItem"><a href="#">New Expenditure</a></li>
           <li className="listItem"><a href="#">Going to 4</a></li>
           <li className="listItem"><a href="#">Going to 5</a></li>
         </ul>
