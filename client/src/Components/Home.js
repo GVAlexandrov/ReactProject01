ef9ffc9ec1144a422b0ff39e0c0e57f580b31f0c
gvalexandrov <gvalexandrov@abv.bg>
2021-04-10 18:13:29 +0300

    Separated HomeGuest from HomeUser

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,11 +1,27 @@
+import { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom'
 
 const Home = () => {
+    const [isLogged, setIsLogged] = useState(false);
+    const uid = localStorage.uid;
+
+    useEffect(() => {
+        if (uid) {
+            setIsLogged(true)
+        } else {
+            setIsLogged(false)
+        }
+    }, [uid])
+
     return (
         <main>
             <p className="guest-message">MoneySafe? The best expense tracker tool of all time! </p>
-            
-            <p className="guest-message">Аre you new here? <NavLink to="/register">Register</NavLink> yourself!</p>
+
+            {
+                isLogged
+                ? <></>
+                : <p className="guest-message">Аre you new here? <NavLink to="/register">Register</NavLink> yourself!</p>
+            }
         </main>
     );
 };
