8a1148e3c8b4d0343abaeef6ee8fe731c231783f
gvalexandrov <gvalexandrov@abv.bg>
2021-04-10 18:11:16 +0300

    AuthValidation added with ErrorHandling

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,17 +1,46 @@
-import { login, activeUser } from './services/authService';
+import { useState } from 'react';
 
+import { login, activeUser } from './services/authService';
+import validate from '../validations/authValidations';
 
 const Login = ({
   history
 }) => {
+  const [emailError, setEmailError] = useState("");
+  const [passError, setPassError] = useState("");
+
+  const [credentials, setCredentials] = useState({
+    email: '',
+    pass: ''
+  });
+
   const onLogin = (e) => {
     e.preventDefault();
-    const email = e.target.elements.email.value;
-    const pass = e.target.elements.pass.value;
+    // const email = e.target.elements.email.value;
+    // const pass = e.target.elements.pass.value;
 
-    login(email, pass)
+    const emailError = validate.email(credentials.email);
+    const passError = validate.password(credentials.pass);
+
+    if (emailError) {
+        setEmailError(emailError);
+        setTimeout(() => {
+            setEmailError('');
+        }, 1500);
+    }
+
+    if (passError) {
+        setPassError(passError);
+        setTimeout(() => {
+            setPassError('');
+        }, 1500);
+    }
+
+    if (emailError || passError) return;
+
+    login(credentials.email, credentials.pass)
       .then(res => activeUser(res.user.uid, res.user.email))
-      .then(res => history.push('/expenses'));
+      .then(() => history.push('/expenses'));
   }
 
 
@@ -22,10 +51,28 @@ const Login = ({
 
         <p>Personal info</p>
         <label htmlFor="email">Email</label>
-        <input id="email" type="text" placeholder="JustMyself123..." autoComplete="on" name="email"/>
+        <input id="email" type="text" placeholder="JustMyself123..." autoComplete="on" value={credentials.email} onInput={(e) => setCredentials(oldState => ({ ...oldState, email: e.target.value}) )} />
+
+        {emailError
+            ? (
+                <div className="notifications" >
+                    <p className="notification-message">{emailError}</p>
+                </div>
+            )
+            : (<></>)
+        }
 
         <label htmlFor="pass">Password</label>
-        <input id="pass" type="password" placeholder="********" autoComplete="on" name="pass"/>
+        <input id="pass" type="password" placeholder="********" autoComplete="on" value={credentials.pass} onInput={(e) => setCredentials(oldState => ({ ...oldState, pass: e.target.value}) )} />
+
+        {passError
+            ? (
+                <div className="notifications" >
+                    <p className="notification-message">{passError}</p>
+                </div>
+            )
+            : (<></>)
+        }
 
         <button type="submit">Login</button>
       </form>
--- client/src/Components/Register.js
+++ client/src/Components/Register.js
@@ -1,18 +1,64 @@
+import { useState } from 'react';
 import { register, activeUser } from './services/authService';
-
+import { refill } from './services/services';
+import validate from '../validations/authValidations';
 
 const Register = ({
     history
 }) => {
+    const [emailError, setEmailError] = useState("");
+    const [passError, setPassError] = useState("");
+    const [repassError, setRepassError] = useState("");
+    const [amountError, setAmountError] = useState("");
 
     const onRegister = (e) => {
         e.preventDefault();
         let email = e.target.elements.email.value;
         let pass = e.target.elements.pass.value;
+        let repass = e.target.elements.repass.value;
+        let amount = e.target.elements.amount.value;
+
+        const emailError = validate.email(email);
+        const passError = validate.password(pass);
+        const repassError = validate.repassword(repass, pass);
+        const amountError = validate.amount(amount);
+
+        if (emailError) {
+            setEmailError(emailError);
+            setTimeout(() => {
+                setEmailError('');
+            }, 1500);
+        }
+
+        if (passError) {
+            setPassError(passError);
+            setTimeout(() => {
+                setPassError('');
+            }, 1500);
+        }
+
+        if (repassError) {
+            setRepassError(repassError);
+            setTimeout(() => {
+                setRepassError('');
+            }, 1500);
+        }
+
+        if (amountError) {
+            setAmountError(amountError);
+            setTimeout(() => {
+                setAmountError('');
+            }, 1500);
+        }
+
+        if (emailError || passError || repassError || amountError) return;
 
         register(email, pass)
-            .then(res => activeUser(res.user.uid, res.user.email))
-            .then(res => history.push('/expenses'))
+            .then(res => {
+                activeUser(res.user.uid, res.user.email);
+                return refill(amount);
+            })
+            .then(() => history.push('/expenses'))
             .catch(error => console.log(error));
     }
 
@@ -26,15 +72,54 @@ const Register = ({
                 <label htmlFor="email">E-mail</label>
                 <input id="email" type="text" placeholder="JustMyself123..." autoComplete="on" name="email" />
 
+                {emailError
+                    ? (
+                        <div className="notifications" >
+                            <p className="notification-message">{emailError}</p>
+                        </div>
+                    )
+                    : (<></>)
+                }
+
                 <label htmlFor="pass">Password</label>
                 <input id="pass" type="password" placeholder="******" autoComplete="on" name="pass" />
 
+                {passError
+                    ? (
+                        <div className="notifications" >
+                            <p className="notification-message">{passError}</p>
+                        </div>
+                    )
+                    : (<></>)
+                }
+
                 <label htmlFor="repeat-password">Repeat Password</label>
-                <input id="repeat-password" type="password" placeholder="******" autoComplete="on" />
+                <input id="repeat-password" type="password" placeholder="******" autoComplete="on" name="repass" />
+
+                {repassError
+                    ? (
+                        <div className="notifications" >
+                            <p className="notification-message">{repassError}</p>
+                        </div>
+                    )
+                    : (<></>)
+                }
+
                 <hr />
+
                 <p>Account</p>
+
                 <label htmlFor="amount">Amount</label>
-                <input id="amount" type="text" placeholder="$125.90" />
+                <input id="amount" type="text" placeholder="$125.90" name="amount" />
+
+                {amountError
+                    ? (
+                        <div className="notifications" >
+                            <p className="notification-message">{amountError}</p>
+                        </div>
+                    )
+                    : (<></>)
+                }
 
                 <button type="submit">Register</button>
             </form>
